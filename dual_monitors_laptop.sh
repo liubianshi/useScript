c9cb7f47ae860c5d75784f92a89ce5e694659ec4
liubianshi_laptop <liu.bian.shi@gmail.com>
2019-12-02 11:09:32 +0800

    dual monitors under i3wm

---
+++ dual_monitors_laptop.sh
@@ -0,0 +1,4 @@
+xrandr --dpi 210 --fb 5440x1620 \
+    --output DP1 --scale 1.5x1.5 --mode 1920x1080 \
+    --pos 0x0 --panning 2880x1620+0+0 \
+    --output eDP1 --mode 2560x1440 --right-of DP1 --panning 2560x1440+2880+0
